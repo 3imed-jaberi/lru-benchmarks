2444aa2bc9904c4d6cfef585ff3605ff54382c55
3imed-jaberi <imed_jebari@hotmail.fr>
2021-01-20 19:13:58 +0100

    update bench pkgs list 🧰 ..

--- packages.js
+++ packages.js
@@ -1,11 +1,16 @@
 // packages list
 const packages = {
   'tiny-lru': { url: 'https://npmjs.com/package/tiny-lru' },
-  hashlru: { url: 'https://npmjs.com/package/hashlru' }
+  'hashlru': { url: 'https://npmjs.com/package/hashlru' },
+  'mnemonist-object': { url: 'https://www.npmjs.com/package/mnemonist' },
+  'lru-cache': { url: 'https://npmjs.com/package/lru-cache' },
+  'js-lru': { url: 'https://www.npmjs.com/package/js-lru' },
+  // 'quick-lru': { url: 'https://npmjs.com/package/quick-lru' },
+  'ylru': { url: 'https://npmjs.com/package/ylru' }
 }
 
 // packages name list.
 const caches = Object.keys(packages)
 
 // export
-module.exports = { packages, caches }
+export default { packages, caches }
